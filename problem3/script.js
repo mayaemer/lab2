f4259b9f87b3c87bd2aac7469b37762b1ca88214
mayaemer <71705519+mayaemer@users.noreply.github.com>
2022-10-27 12:09:32 +0100

    comments added

--- problem3/script.js
+++ problem3/script.js
@@ -1,16 +1,20 @@
+// when user searches github user, clicking search button, function runs
 document.getElementById('searchBtn').addEventListener('click', async function(){
     event.preventDefault();
+
+    //display section class elements
     let sections = document.getElementsByClassName('section');
-    
     for (let section of sections){
         section.style.display = 'grid';
     }
 
+    //get input from search bar and send request using api with username, parsing the returned data to json
     let searchUser = document.getElementById('userName').value;
     const res = await fetch('https://api.github.com/users/' + searchUser);
     let userData = await res.json()
 
 
+    // assign variable to each piece of data returned, and add it to html elements
     let image = userData.avatar_url;
     let username = userData.login;
     let email = userData.email;
@@ -21,18 +25,16 @@ document.getElementById('searchBtn').addEventListener('click', async function(){
                         <p> Email: ` + email + `</p>
                         <p> Location: ` + location + `</p>
                         <p> Number of Gists: ` + gists + `</p>`;
-                
-    
+            
     document.getElementById('avatarSection').innerHTML = avatarSection;
     document.getElementById('infoSection').innerHTML = infoSection;
     
-
-    let repos = userData.repos_url;
-    console.log(repos);
-    
+    // fetch repo data, parse it to json 
+    let repos = userData.repos_url;    
     const response = await fetch(repos);
     let repo_res = await response.json(); 
 
+    // create table for repo info
     let repoTableBody= `<thead>
                     <tr>
                         <th>Name</th>
@@ -43,6 +45,7 @@ document.getElementById('searchBtn').addEventListener('click', async function(){
                     </tbody>`
     document.getElementById('repoTable').innerHTML = repoTableBody;
     
+    // for each repo in response, assign to variable and add into repo table
     repo_res.forEach( repo_res => {
         let repoName = repo_res.name;
         let repoDesc = repo_res.description;
@@ -56,6 +59,7 @@ document.getElementById('searchBtn').addEventListener('click', async function(){
     }
     )
 
+    // if repo table has more than 5 rows, add class scrollable, which will add scroll to table using css
     let repoRows = document.getElementById('repoTable').rows.length;
     //console.log(repoRows)
     if (repoRows > 5){
